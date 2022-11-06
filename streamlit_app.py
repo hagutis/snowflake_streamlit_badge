9b6f03db21d4634fa860632d3b6cd0f9ef87053c
hagutis <53209183+hagutis@users.noreply.github.com>
2022-11-06 01:12:37 -0700

    Update streamlit_app.py
    fixed another bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 streamlit.header('FruityVice Fruit Advice!!!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?'
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
                                      
 # take the json version of the response and normalize it
